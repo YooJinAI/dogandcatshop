0fd9178913c0b44160901997bbb23255a183f93b
yoojin <dbwlsakstp1@gmail.com>
2025-04-08 15:19:22 +0900

    test branch

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -233,6 +233,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
           color: Colors.white,
           boxShadow: [
             BoxShadow(
+              // ignore: deprecated_member_use
               color: Colors.grey.withOpacity(0.2),
               spreadRadius: 1,
               blurRadius: 5,
